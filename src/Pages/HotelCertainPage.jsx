684f4876e836576e9ada7875f1509826b738f84a
choyy <choyyein5@gmail.com>
2025-01-07 19:34:59 +0800

    update the data

--- src/Pages/HotelCertainPage.jsx
+++ src/Pages/HotelCertainPage.jsx
@@ -20,7 +20,7 @@ const fetchHotelData = (id) => {
 const HotelCertainPage = () => {
   const { id } = useParams();
   const [hotel, setHotel] = useState(null);
-  const [filteredRooms, setFilteredRooms] = useState([]); // Separate state for filtered rooms
+  const [filteredRooms, setFilteredRooms] = useState([]);
 
   const [filters, setFilters] = useState({
     priceRange: [],
@@ -34,10 +34,13 @@ const HotelCertainPage = () => {
   });
 
   useEffect(() => {
+    // Scroll to the top of the page when the component is mounted
+    window.scrollTo(0, 0);
+
     const fetchedHotel = fetchHotelData(id);
     setHotel(fetchedHotel);
     if (fetchedHotel) {
-      setFilteredRooms(fetchedHotel.rooms); // Initialize filtered rooms
+      setFilteredRooms(fetchedHotel.rooms);
     }
   }, [id]);
 
@@ -54,9 +57,7 @@ const HotelCertainPage = () => {
         );
       }
 
-      // Add more filtering logic here if needed
-
-      setFilteredRooms(rooms); // Update filtered rooms instead of hotel
+      setFilteredRooms(rooms);
     };
 
     applyFilters();
@@ -71,25 +72,30 @@ const HotelCertainPage = () => {
       <div className="mb-24"></div>
 
       {/* Photo Gallery */}
-      <div className="photo-gallery flex md:flex-row flex-col md:gap-4 gap-2 md:mb-8 mb-4">
-        <div className="main-image md:w-3/6 w-full">
+      <div
+        id="top"
+        className="photo-gallery grid grid-cols-1 md:grid-cols-5 gap-4 md:mb-8 mb-4 max-h-[533px] overflow-hidden"
+      >
+        {/* Main Image */}
+        <div className="main-image col-span-1 md:col-span-3 max-h-[533px] overflow-hidden">
           <img
             src={hotel.images[0]}
             alt={`${hotel.name} main view`}
-            className="w-full h-[101%] rounded-md"
+            className="w-full h-full rounded-md object-cover"
+            loading="lazy" // Ensure lazy loading is supported for faster load
           />
         </div>
-        <div className="side-images md:w-[50%]">
-          <div className="grid md:grid-cols-2 grid-cols-4 md:gap-4 gap-2">
-            {hotel.images.slice(1, 5).map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                alt={`${hotel.name} view ${index + 2}`}
-                className=" h-auto rounded-md"
-              />
-            ))}
-          </div>
+        {/* Side Images */}
+        <div className="side-images grid md:grid-cols-2 grid-cols-4 md:col-span-2 gap-4">
+          {hotel.images.slice(1, 5).map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`${hotel.name} view ${index + 2}`}
+              className="w-full h-full rounded-md object-cover max-h-[533px]"
+              loading="lazy"
+            />
+          ))}
         </div>
       </div>
 
@@ -126,7 +132,7 @@ const HotelCertainPage = () => {
           )}
           {hotel.amenities.includes("wifi") && (
             <li className="flex flex-col items-center justify-center">
-              <FaWifi className="w-12 mb-2 text-blue-500" />{" "}
+              <FaWifi className="w-12 mb-2 text-blue-500" />
               <p className="text-sm">Free Wi-Fi</p>
             </li>
           )}
@@ -151,19 +157,20 @@ const HotelCertainPage = () => {
         {filteredRooms.map((room, index) => (
           <div
             key={index}
-            className="room-option bg-white text-black p-4 rounded-md mb-4 flex flex-row gap-4"
+            className="room-option bg-white text-black rounded-md mb-4 flex flex-row gap-4"
           >
             {/* Room Image */}
             <div className="room-image lg:w-1/5 w-2/5">
               <img
-                src={room.image}
+                src={room.photo}
                 alt={`${room.type} view`}
-                className="w-full h-auto rounded-md"
+                className="w-full h-full rounded-l-md"
+                loading="lazy"
               />
             </div>
 
             {/* Room Details */}
-            <div className="room-details md:w-3/5">
+            <div className="room-details md:w-3/5 p-4">
               <h3 className="font-bold text-lg mb-2">{room.type}</h3>
               <ul className="text-md mb-4">
                 {room.benefits.map((benefit, idx) => (
@@ -171,24 +178,42 @@ const HotelCertainPage = () => {
                 ))}
               </ul>
               <div className="flex items-center gap-2 text-gray-700">
-                <FaUser className="" /> <span>{room.adults} Adults</span>
-                <FaChild /> <span>{room.children} Children</span>
+                <FaUser /> <span>{room.adults} Adults</span>
+                {room.children > 0 && (
+                  <>
+                    <FaChild /> <span>{room.children} Children</span>
+                  </>
+                )}
               </div>
               <div className="md:hidden text-justify">
                 <p className="font-bold text-xl my-2">
                   RM {room.price}{" "}
-                  <span className="font-normal text-base ">/ per night</span>
+                  <span className="font-normal text-base">/ per night</span>
                 </p>
-                <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+                <button
+                  className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                  onClick={() =>
+                    room.link
+                      ? window.location.assign(room.link)
+                      : alert("This room is not available now!")
+                  }
+                >
                   Book Now
                 </button>
               </div>
             </div>
 
             {/* Price and Booking */}
-            <div className="room-booking lg:w-1/5 md:w-2/5 text-center items-center justify-center p-8 hidden md:block">
+            <div className="room-booking lg:w-1/5 md:w-2/5 text-center items-center justify-center p-8 py-14 hidden md:block">
               <p className="font-bold text-xl mb-2">RM {room.price}</p>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                onClick={() =>
+                  room.link
+                    ? window.location.assign(room.link)
+                    : alert("This room is not available now!")
+                }
+              >
                 Book Now
               </button>
             </div>
@@ -196,8 +221,8 @@ const HotelCertainPage = () => {
         ))}
       </div>
       <div>
-        <h2 className="text-2xl font-bold mt-6 mb-4">Location </h2>
-        <GoogleMapEmbed mapLink={hotel.googleMapsLink} />
+        <h2 className="text-2xl font-bold mt-6 mb-4">Location</h2>
+        <GoogleMapEmbed mapLink={hotel.googleMap} />
       </div>
       <RelatedHotel hotels={hotelData} />
     </div>
